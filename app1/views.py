176aede6c4f1c4aadde222821c84447b9e2e56cc
PrashantDas <78946070+PrashantDas@users.noreply.github.com>
2023-07-31 13:46:44 +0530

    Add files via upload

---
+++ app1/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'app1/home.html', {})
+
+def contact(request):
+    return render(request, 'app1/contact.html', {})
+
+def projects(request):
+    return render(request, 'app1/projects.html', {})
+
+def games(request):
+    return render(request, 'app1/games.html', {})
